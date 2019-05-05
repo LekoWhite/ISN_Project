e339910e3be5220ac07f444bc71c7c496613d58d
Loucas CUBEDDU <lcubeddu@outlook.fr>
2019-05-05 15:55:09 +0200

    Resolve conflict

--- js/play.js
+++ js/play.js
@@ -1,22 +1,4 @@
 let cards = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-  {id: 0, first_coords: [0, 0], second_coords: [0, 0], url: '../images/ram.jpg'}
-    {id: 1, first_coords: [0, 0], second_coords: [0, 0], url: '../images/dany.png'}
-    {id: 2, first_coords: [0, 0], second_coords: [0, 0], url: '../images/rin.jpeg'}
-    {id: 3, first_coords: [0, 0], second_coords: [0, 0], url: '../images/violet.jpeg'}
-    {id: 4, first_coords: [0, 0], second_coords: [0, 0], url: '../images/dustin.png'}
-    {id: 5, first_coords: [0, 0], second_coords: [0, 0], url: '../images/grumpy_cat.jpg'}
-    {id: 6, first_coords: [0, 0], second_coords: [0, 0], url: '../images/guts.jpg'}
-    {id: 7, first_coords: [0, 0], second_coords: [0, 0], url: '../images/macron.jpg'}
-    {id: 8, first_coords: [0, 0], second_coords: [0, 0], url: '../images/mirorin.jpg'}
-    {id: 9, first_coords: [0, 0], second_coords: [0, 0], url: '../images/rem.jpg'}
-    {id: 10, first_coords: [0, 0], second_coords: [0, 0], url: '../images/saber.jpg'}
-    {id: 11, first_coords: [0, 0], second_coords: [0, 0], url: '../images/zero_two.jpg'}
-    {id: 12, first_coords: [0, 0], second_coords: [0, 0], url: '../images/izuku.jpg'}
-    {id: 13, first_coords: [0, 0], second_coords: [0, 0], url: '../images/arya.jpg'}
-    {id: 14, first_coords: [0, 0], second_coords: [0, 0], url: '../images/goku.jpg'}
-=======
     {id: 0, first_coords: [0, 0], second_coords: [0, 0], url: '../images/ram.jpg'},
     {id: 1, first_coords: [0, 0], second_coords: [0, 0], url: '../images/dany.png'},
     {id: 2, first_coords: [0, 0], second_coords: [0, 0], url: '../images/rin.jpeg'},
@@ -32,7 +14,6 @@ let cards = [
     {id: 12, first_coords: [0, 0], second_coords: [0, 0], url: '../images/izuku.jpg'},
     {id: 13, first_coords: [0, 0], second_coords: [0, 0], url: '../images/arya.jpg'},
     {id: 14, first_coords: [0, 0], second_coords: [0, 0], url: '../images/goku.jpg'},
->>>>>>> 24772cab48cfdd8fd2fb13904f6d8e8288e6f39a
     {id: 15, first_coords: [0, 0], second_coords: [0, 0], url: '../images/america.jpg'}
 ];
 
