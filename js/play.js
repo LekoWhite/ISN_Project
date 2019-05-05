4b52edc9abb4fd45b43b146832d5ff68ec035466
Loucas CUBEDDU <lcubeddu@outlook.fr>
2019-05-05 12:16:44 +0200

    Fix bad syntax of the cards array

--- js/play.js
+++ js/play.js
@@ -1,19 +1,19 @@
 let cards = [
-  {id: 0, first_coords: [0, 0], second_coords: [0, 0], url: ''}
-    {id: 1, first_coords: [0, 0], second_coords: [0, 0], url: '../images/dany.png'}
-    {id: 2, first_coords: [0, 0], second_coords: [0, 0], url: '../images/rin.jpeg'}
-    {id: 3, first_coords: [0, 0], second_coords: [0, 0], url: '../images/violet.jpeg'}
-    {id: 4, first_coords: [0, 0], second_coords: [0, 0], url: '../images/dustin.png'}
-    {id: 5, first_coords: [0, 0], second_coords: [0, 0], url: '../images/grumpy_cat.jpg'}
-    {id: 6, first_coords: [0, 0], second_coords: [0, 0], url: '../images/guts.jpg'}
-    {id: 7, first_coords: [0, 0], second_coords: [0, 0], url: '../images/macron.jpg'}
-    {id: 8, first_coords: [0, 0], second_coords: [0, 0], url: '../images/mirorin.jpg'}
-    {id: 9, first_coords: [0, 0], second_coords: [0, 0], url: '../images/rem_ram.jpg'}
-    {id: 10, first_coords: [0, 0], second_coords: [0, 0], url: '../images/saber.jpg'}
-    {id: 11, first_coords: [0, 0], second_coords: [0, 0], url: '../images/zero_two.jpg'}
-    {id: 12, first_coords: [0, 0], second_coords: [0, 0], url: '../images/izuku.jpg'}
-    {id: 13, first_coords: [0, 0], second_coords: [0, 0], url: '../images/'}
-    {id: 14, first_coords: [0, 0], second_coords: [0, 0], url: '../images/'}
+  {id: 0, first_coords: [0, 0], second_coords: [0, 0], url: ''},
+    {id: 1, first_coords: [0, 0], second_coords: [0, 0], url: '../images/dany.png'},
+    {id: 2, first_coords: [0, 0], second_coords: [0, 0], url: '../images/rin.jpeg'},
+    {id: 3, first_coords: [0, 0], second_coords: [0, 0], url: '../images/violet.jpeg'},
+    {id: 4, first_coords: [0, 0], second_coords: [0, 0], url: '../images/dustin.png'},
+    {id: 5, first_coords: [0, 0], second_coords: [0, 0], url: '../images/grumpy_cat.jpg'},
+    {id: 6, first_coords: [0, 0], second_coords: [0, 0], url: '../images/guts.jpg'},
+    {id: 7, first_coords: [0, 0], second_coords: [0, 0], url: '../images/macron.jpg'},
+    {id: 8, first_coords: [0, 0], second_coords: [0, 0], url: '../images/mirorin.jpg'},
+    {id: 9, first_coords: [0, 0], second_coords: [0, 0], url: '../images/rem_ram.jpg'},
+    {id: 10, first_coords: [0, 0], second_coords: [0, 0], url: '../images/saber.jpg'},
+    {id: 11, first_coords: [0, 0], second_coords: [0, 0], url: '../images/zero_two.jpg'},
+    {id: 12, first_coords: [0, 0], second_coords: [0, 0], url: '../images/izuku.jpg'},
+    {id: 13, first_coords: [0, 0], second_coords: [0, 0], url: '../images/'},
+    {id: 14, first_coords: [0, 0], second_coords: [0, 0], url: '../images/'},
     {id: 15, first_coords: [0, 0], second_coords: [0, 0], url: '../images/'}
 ];
 
